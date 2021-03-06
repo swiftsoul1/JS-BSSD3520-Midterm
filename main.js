5e351e2ecad09b6517a3af8b3c7e638778a97a6b
Jonah <47265356+swiftsoul1@users.noreply.github.com>
2021-03-06 13:11:13 -0700

    numer input

--- main.js
+++ main.js
@@ -8,14 +8,63 @@ document.addEventListener('DOMContentLoaded', () => {
     
     createSelect(classes, 'classes');
     createSelect(races, 'races');
+    createNumInput('Strength')
 });
 
-
-
 //attempting to use functions as 'fragments'
+//numInput for skills
+const createNumInput = (name) => {
+     //container
+    let div = document.createElement('div');
+        div.id = name;
+        div.classList.add('row');
+        div.classList.add('border');
+        div.classList.add('justify-content-center');
+        div.classList.add('p-5');
+        div.classList.add('m-2');
+    document.getElementById('content').append(div);
+    //input
+    let input = document.createElement('input');
+        input.type = 'number';
+        input.max = 18;
+        input.min = 3;
+        input.name = name;
+        input.id = name;
+    div.append(input);
+    //roll button
+    let roll = document.createElement('button');
+        roll.id = name;
+        roll.innerHTML = 'roll';
+        roll.classList.add('button-primary');
+        roll.addEventListener('click', () => {
+            //roll 3 dice, as the rules dictate
+            let roll = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);  
+            input.value = roll;
+        });
+     div.append(roll);
+    //submit button
+    let submit = document.createElement('button');
+        submit.id = name;
+        submit.innerHTML = 'submit';
+        submit.classList.add('button-primary');
+        submit.addEventListener('click', () => {
+            removeElement(name);
+        });
+     div.append(submit);
+}
+
 //select 
 const createSelect = (options, name) => {
-    //create classSelect input and options
+    //container
+    let div = document.createElement('div');
+        div.id = name;
+        div.classList.add('row');
+        div.classList.add('border');
+        div.classList.add('justify-content-center');
+        div.classList.add('p-5');
+        div.classList.add('m-2');
+    document.getElementById('content').append(div);
+    //select/options
     let sel = document.createElement('select');
         sel.id = name;
         sel.name = name;
@@ -28,9 +77,9 @@ const createSelect = (options, name) => {
             sel.append(option);
     }
     //add select after its created
-    document.getElementById('content').append(sel);
+    div.append(sel);
     
-    //submitclass button
+    //submit button
     let submit = document.createElement('button');
         submit.id = name;
         submit.innerHTML = 'submit';
@@ -38,9 +87,10 @@ const createSelect = (options, name) => {
         submit.addEventListener('click', () => {
             removeElement(name);
         });
-     document.getElementById('content').append(submit);
+     div.append(submit);
 }
 
+//removeById
 const removeElement = (name) => {
     while(document.getElementById(name)) document.getElementById(name).remove(); 
 }
