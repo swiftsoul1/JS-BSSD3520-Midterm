70d782306bec4b983bb5c1fb916a0c307d02d714
Jonah <47265356+swiftsoul1@users.noreply.github.com>
2021-03-05 17:42:50 -0700

    fragment like functions?

--- main.js
+++ main.js
@@ -1,27 +1,47 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-    createClassesSelect();
+    //dnd classes
+    const classes =['fighter', 'cleric', 'warlock', 'druid', 'paladin', 'sorceror', 'wizard', 'rogue'];
+    //dnd races
+    const races = ['gnome', 'dwarf', 'human', 'orc', 'halfling', 'elf', 'teifling'];
+    
+    createSelect(classes, 'classes');
+    createSelect(races, 'races');
 });
 
 
 
-const createClassesSelect = () => {
-     //dnd classes
-    const classes =['fighter', 'cleric', 'warlock', 'druid', 'paladin', 'sorceror', 'wizard', 'rogue'];
+//attempting to use functions as 'fragments'
+//select 
+const createSelect = (options, name) => {
     //create classSelect input and options
-    let classSel = document.createElement('select');
-        classSel.classList.add('selectpicker');
-        classSel.id = 'classes';
-        classSel.name = 'classes';
-    for(let i = 0;i < classes.length;i++){
+    let sel = document.createElement('select');
+        sel.id = name;
+        sel.name = name;
+    for(let i = 0;i < options.length;i++){
         //create option and add
-        let cOption = document.createElement('option');
-            cOption.id = classes[i];
-            cOption.value = classes[i];
-            cOption.innerHTML = classes[i];
-            classSel.append(cOption);
+        let option = document.createElement('option');
+            option.id = options[i];
+            option.value = options[i];
+            option.innerHTML = options[i];
+            sel.append(option);
     }
     //add select after its created
-    document.getElementById('content').append(classSel);    
+    document.getElementById('content').append(sel);
+    
+    //submitclass button
+    let submit = document.createElement('button');
+        submit.id = name;
+        submit.innerHTML = 'submit';
+        submit.classList.add('button-primary');
+        submit.addEventListener('click', () => {
+            removeElement(name);
+        });
+     document.getElementById('content').append(submit);
 }
+
+const removeElement = (name) => {
+    while(document.getElementById(name)) document.getElementById(name).remove(); 
+}
+
