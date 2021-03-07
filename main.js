133812e15de7069873eed18c4a943bdf6ea6bce7
Jonah <47265356+swiftsoul1@users.noreply.github.com>
2021-03-07 01:53:38 -0700

    finishing touches

--- main.js
+++ main.js
@@ -1,90 +1,96 @@
 'use strict';
 
+//object
+const newCharacter = () => {
 /*used this example for detecting object changes
 https://www.codegrepper.com/code-examples/javascript/how+to+listen+for+variable+changes+js*/
 //changed example to use my variables and to detect when all variables are filled
-let character = {
-    name: '',
-    class: '',
-    race: '',
-    strength: 0,
-    dexterity: 0,
-    constitution: 0,
-    intelligence: 0,
-    wisdom: 0,
-    charisma: 0,
-    checkFilled() {
-        //check if the user has filled in all the fields
-        if(this.name !== '' && this.class !== '' && this.race !== '' && this.strength >= 3 && this.constitution >= 3 && this.dexterity >= 3 && this.intelligence >= 3 && this.wisdom >= 3 && this.charisma >= 3){
+    let character = {
+        name: '',
+        class: '',
+        race: '',
+        strength: 0,
+        dexterity: 0,
+        constitution: 0,
+        intelligence: 0,
+        wisdom: 0,
+        charisma: 0,
+        checkFilled() {
+            //check if the user has filled in all the fields
+            if(this.name !== '' && this.class !== '' && this.race !== '' && this.strength >= 3 && this.constitution >= 3 && this.dexterity >= 3 && this.intelligence >= 3 && this.wisdom >= 3 && this.charisma >= 3){
            createDetailPage();
-        }
-    },
-    get getName() {
-        return this.name;
-    },
-    set setName(value) {
-        this.name = value;
-        this.checkFilled();
-    },
-    get getClass() {
-        return this.class;
-    },
-    set setClass(value) {
-        this.class = value;
-        this.checkFilled();
-    },
-    get getRace() {
-        return this.race;
-    },
-    set setRace(value) {
-        this.race = value;
-        this.checkFilled();
-    },
-    get getStrength() {
-        return this.strength;
-    },
-    set setStrength(value) {
-        this.strength = value;
-        this.checkFilled();
-    },
-    get getDexterity() {
-        return this.dexterity;
-    },
-    set setDexterity(value) {
-        this.dexterity = value;
-        this.checkFilled();
-    },
-    get getConstitution() {
+            }
+        },
+        get getName() {
+            return this.name;
+        },
+        set setName(value) {
+            this.name = value;
+            this.checkFilled();
+        },
+        get getClass() {
+            return this.class;
+        },
+        set setClass(value) {
+            this.class = value;
+            this.checkFilled();
+        },
+        get getRace() {
+            return this.race;
+        },
+        set setRace(value) {
+            this.race = value;
+            this.checkFilled();
+        },
+        get getStrength() {
+            return this.strength;
+        },
+        set setStrength(value) {
+            this.strength = value;
+            this.checkFilled();
+        },
+        get getDexterity() {
+            return this.dexterity;
+        },
+        set setDexterity(value) {
+            this.dexterity = value;
+            this.checkFilled();
+        },
+        get getConstitution() {
         return this.constitution;
-    },
-    set setConstitution(value) {
-        this.constitution = value;
-        this.checkFilled();
-    },
-    get getIntelligence() {
-        return this.intelligence;
-    },
-    set setIntelligence(value) {
-        this.intelligence = value;
-        this.checkFilled();
-    },
-    get getWisdom() {
-        return this.wisdom;
-    },
-    set setWisdom(value) {
-        this.wisdom = value;
-        this.checkFilled();
-    },
-    get getCharisma() {
+        },
+        set setConstitution(value) {
+            this.constitution = value;
+            this.checkFilled();
+        },
+        get getIntelligence() {
+            return this.intelligence;
+        },
+        set setIntelligence(value) {
+            this.intelligence = value;
+            this.checkFilled();
+        },
+        get getWisdom() {
+            return this.wisdom;
+        },
+        set setWisdom(value) {
+            this.wisdom = value;
+            this.checkFilled();
+        },
+        get getCharisma() {
         return this.charisma;
-    },
-    set setCharisma(value) {
-        this.charisma = value;
-        this.checkFilled();
-    },
+        },
+        set setCharisma(value) {
+            this.charisma = value;
+            this.checkFilled();
+        },
    
+    }
+    return character;
 }
 /*end object*/
+
+let character = newCharacter();
 
 document.addEventListener('DOMContentLoaded', () => {
     createStartPage();
@@ -109,65 +115,50 @@ const createStartPage =() => {
 
 //detail page
 const createDetailPage = () => {
+    //variables
     let name = 'detail';
+    let div2, p;
+    let labels = ['name', 'race', 'class', 'strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma'];
+    let values = [character.getName, character.getRace, character.getClass, character.getStrength, character.getDexterity, character.getConstitution, character.getIntelligence, character.getWisdom, character.getCharisma];
     //container
     let div = containerDiv(name);
     document.getElementById('content').append(div);
     
+    //image
+    div2 = containerDiv(name);
+    let image = document.createElement('img');
+        image.src = './images/' + character.getRace + '.jpg';
+        image.id = name;
+        image.classList.add("img-fluid");
+    div.append(image);
     //stats
-    let div2 = containerDiv(name);
-    let p = document.createElement('p');
-        p.innerHTML = 'name: ' + character.getName;
-    div2.append(p);
-    div.append(div2)
-    
-    div2 = containerDiv(name);
-    p = document.createElement('p');
-    p.innerHTML = 'race: ' + character.getRace;
-    div2.append(p);
-    div.append(div2);
-    
-    div2 = containerDiv(name);
-    p = document.createElement('p');
-    p.innerHTML = 'class: ' + character.getClass;
-    div2.append(p);
-    div.append(div2);
-    
-    div2 = containerDiv(name);
-    p = document.createElement('p');
-    p.innerHTML = 'strength: ' + character.getStrength;
-    div2.append(p);
-    div.append(div2);
-    
-    div2 = containerDiv(name);
-    p = document.createElement('p');
-    p.innerHTML = 'dexterity: ' + character.getDexterity;
-    div2.append(p);
-    div.append(div2);
-    
-    div2 = containerDiv(name);
-    p = document.createElement('p');
-        p.innerHTML = 'constitution: ' + character.getConstitution;
-    div2.append(p);
-    div.append(div2)
-    
-    div2 = containerDiv(name);
-    p = document.createElement('p');
-    p.innerHTML = 'intelligence: ' + character.getIntelligence;
-    div2.append(p);
-    div.append(div2);
-    
-    div2 = containerDiv(name);
-    p = document.createElement('p');
-    p.innerHTML = 'wisdom: ' + character.getWisdom;
-    div2.append(p);
-    div.append(div2);
-    
-    div2 = containerDiv(name);
-    p = document.createElement('p');
-    p.innerHTML = 'charisma: ' + character.getCharisma;
-    div2.append(p);
-    div.append(div2);
+    for(let i = 0; i<values.length;i++){
+        div2 = containerDiv(name);
+        div2.style.backgroundColor = 'yellow';
+        div2.style.height = '3em';
+        div2.style.width = 'auto';
+        p = document.createElement('p');
+        p.innerHTML = labels[i]+ ': ' + values[i];
+        div2.append(p);
+        div.append(div2)
+    }
+    let div3 = containerDiv(name);
+    let refresh = document.createElement('button');
+        refresh.id = name;
+        refresh.innerHTML = 'Create New Character';
+        refresh.classList.add('btn');
+        refresh.classList.add('btn-primary');
+        refresh.addEventListener('click', () => {
+            removeElement('detail');
+            document.body.style.backgroundImage = "";
+            character = newCharacter();
+            createStartPage();
+        });
+    div3.append(refresh);
+    div.append(div3);
+    //styling background based on chosen class 
+    document.body.style.backgroundSize = "cover"; 
+    document.body.style.backgroundImage = "url(./images/"+character.getClass +".jpg)"; 
 }
 
 //text input
@@ -187,7 +178,6 @@ const createNameInput = (name) => {
     //submit button
     let submit = submitBtn(name);
         submit.addEventListener('click', () => {
-            console.log(input.value)
             if(input.value != ''){
                 character.setName = input.value;
                 removeElement(name);
@@ -218,7 +208,9 @@ const createNumInput = (name) => {
     let roll = document.createElement('button');
         roll.id = name;
         roll.innerHTML = 'roll';
-        roll.classList.add('button-primary');
+        roll.classList.add('btn');
+        roll.classList.add('border');
+        roll.classList.add('btn-primary');
         roll.addEventListener('click', () => {
             //roll 3 dice, as the rules dictate n add them up
             let rNum = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);  
@@ -327,4 +319,3 @@ const makeLabel = (name) => {
 const removeElement = (name) => {
     while(document.getElementById(name)) document.getElementById(name).remove(); 
 }
-
